9c5071f3c8b0ac91ecb80b833c4ca44b714f385e
BRIAN <questionb17@gmail.com>
2022-10-18 22:39:09 +0200

    first push

---
+++ index2.js
@@ -0,0 +1,14 @@
+function subscribe(){
+    var username =document.getElementById("username");
+    var email =document.getElementById("e-mail");
+    var name =document.getElementById("name");
+    var mail =document.getElementById("mail");
+    name.innerHTML="Please enter your name";
+    mail.innerHTML="Please enter your email";
+    name.style.color="red";
+    mail.style.color="red";
+}
+
+if(username>=){
+
+}
